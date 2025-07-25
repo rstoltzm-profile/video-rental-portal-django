0545c788613ab4a7b497cce62bc86f25f543f79d
rstoltzm-profile <rstoltzm@gmail.com>
2025-07-25 15:45:28 -0700

    models

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Film(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_year = models.IntegerField()
+    language = models.CharField(max_length=50)
+    rating = models.CharField(max_length=10)
